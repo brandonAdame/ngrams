960723a91b82a751da1f521649b11713cfde0ae3
brandonAdame <brandonadame29@yahoo.com>
2018-01-15 19:02:35 -0500

    First commit

---
+++ src/ngrams.java
@@ -0,0 +1,25 @@
+/**
+ * This program computes the n-gram frequencies for documents in a text
+ * corpus.
+ *
+ * Created by: Brandon A. Gachuz
+ * Date: 1-15-17
+ */
+public class ngrams {
+
+    /*
+    Outline:
+        - Read one file at a time
+        - For each file:
+            - Extract all words in the line
+            - Keep track of unigram/bigram/trigram and their frequency of
+            occurrence
+
+        - For each word:
+            * Extract characters and keep their occurrence count
+
+        - CLOSE THE INPUT FILE
+
+
+     */
+}
